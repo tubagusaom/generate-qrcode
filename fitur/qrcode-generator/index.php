034ac00ed7fbefdd64c4d6357146641a0eb52981
Tera Byte <tubagus.aom.swk@gmail.com>
2023-07-26 12:22:51 +0700

    mergefitur

--- fitur/qrcode-generator/index.php
+++ fitur/qrcode-generator/index.php
@@ -160,7 +160,7 @@
                     <a href="https://itconsultant.biz.id/">ICI</a>. All rights reserved. -->
 
                     <footer class="d-flex flex-wrap justify-content-between align-items-center border-top">
-                        <div class="col-md-4 d-flex align-items-center p-3">
+                        <div class="col-md-4 d-flex align-items-center p-2">
                             <span class="mb-3 mb-md-0 text-body-secondary">
                                 Copyright &copy;
                                 <script>
