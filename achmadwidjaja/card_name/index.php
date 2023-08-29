066f0618d3706d61f8edcab0624990c4d8cd9916
Tera Byte <tubagus.aom.swk@gmail.com>
2023-08-29 09:59:35 +0700

    mergecardname

---
+++ achmadwidjaja/card_name/index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Achmad Widjaja</title>
+
+    <meta name="description" content="Name Card" />
+    <meta name="keywords" content="Biografi, Profil, achmad widjaja, acwid, achmad, widjaja" />
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="../_tera_byte/img/awcsa.png" type="image/x-icon" />
+    <link rel="apple-touch-icon" href="../_tera_byte/img/awcsa-touch-icon.png">
+
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,700,300" rel="stylesheet">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" rel="stylesheet">
+
+    <link href="../_tera_byte/style_1.css" rel="stylesheet">
+</head>
+
+
+<body>
+        <div class="card">
+            <div class="photo"></div>
+            <div class="banner"></div>
+            <ul>
+                <li><b>Achmad Widjaja</b></li>
+                <li style="font-size:14px;padding-top:10px;">President Commisioner & Independent Commissioner</li>
+                <li style="font-size:14px;padding-top:5px;"><b>Astrindo Nusantara Infrastruktur</b></li>
+
+                <br>
+
+                <li style="font-size:14px;padding-top:10px;">Committee Industry Association m.e.c</li>
+                <li style="font-size:14px;padding-top:5px;"><b>KADIN</b></li>
+            </ul>
+            <button class="contact" id="main-button">click for detail information</button>
+            <div class="social-media-banner">
+                <a href=""><i class="fa fa-twitter"></i></a>
+                <a href=""><i class="fa fa-facebook"></i></a>
+                <a href=""><i class="fa fa-instagram"></i></a>
+                <a href=""><i class="fa fa-linkedin"></i></a>
+            </div>
+
+            <form class="email-form">
+
+                <!-- <div class="details">
+                    <div class="col-satu"><i class="fa fa-briefcase"></i> &nbsp; PT CATUR SENTOSA ADIPRANA Tbk</div>
+                </div> -->
+
+                <div class="details" style="margin-top: 15px;">
+                    <i class="fa fa-mobile"></i> &nbsp; &nbsp; &nbsp; +62 813 9999 0707
+                </div>
+
+                <div class="details" style="margin-top: 5px;">
+                    <i class="fa fa-phone"></i> &nbsp; &nbsp; +6221 5081 5252
+                    <br> <font style="padding-left: 32px;font-size:10px;">Astrindo</font>
+                    
+                    <br> <br> <font style="padding-left: 32px;">+6221 527 4484</font>
+                    <br> <font style="padding-left: 32px;font-size:10px;">Kadin</font><br>
+                </div>
+
+                <div class="details" style="margin-top: 65px;">
+                    <i class="fa fa-envelope-o"></i> &nbsp; achmadwidjaja@me.com
+                    <br> <font style="padding-left: 28px;font-size:10px;">Astrindo</font>
+
+                    <br> <br> <font style="padding-left: 28px;">aw@kadin.id</font>
+                    <br> <font style="padding-left: 28px;font-size:10px;">Kadin</font><br>
+                </div>
+
+                <div class="details" style="margin-top: 68px;">
+                    <i class="fa fa-map-marker"></i> &nbsp; Sopo Del Office Tower B. 21 Floor
+                    <br> <font style="padding-left: 20px;">Jl. Mega Kuningan Barat III Jaksel 12950</font>
+                    <br> <font style="padding-left: 20px;font-size:10px;">Astrindo</font>
+
+                    <br> <br> <font style="padding-left: 20px;">Menara Kadin Indonesia Lt. 29</font>
+                    <br> <font style="padding-left: 20px;">Jl. H . Rasuna Said X-5 Kav. 2-3 Jakarta 12950</font>
+                    <br> <font style="padding-left: 20px;font-size:10px;">Kadin</font><br>
+                </div>
+
+                <!-- <button class="contact">send</button> -->
+          </form>
+
+          </div>
+
+    </body>
+
+</html>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script src="../_tera_byte/script.js"></script>
